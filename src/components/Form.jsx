d362267ccacdf39f8353e7d17faa139c978c9eb3
Alex <alexvnep@gmail.com>
2024-10-25 13:48:27 +0200

    feat: creat form to hold input components

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import PersonalDetailsInput from "./PersonalDetailsInput";
+
+function Form() {
+  return (
+    <>
+      <form action="#" method="post">
+        <PersonalDetailsInput />
+      </form>
+    </>
+  );
+}
+
+export default Form;
