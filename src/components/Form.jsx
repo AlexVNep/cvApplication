2c12ba5201236104a13545e8d3761199dcfd7e09
Alex <alexvnep@gmail.com>
2024-10-25 14:12:37 +0200

    feat: add education and work exp to form to render

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,23 @@
 import PersonalDetailsInput from "./PersonalDetailsInput";
+import EducationInput from "./EducationInput";
+import WorkXpInput from "./WorkXpInput";
 
 function Form() {
   return (
     <>
       <form action="#" method="post">
-        <PersonalDetailsInput />
+        <div>
+          <h2>General Information</h2>
+          <PersonalDetailsInput />
+        </div>
+        <div>
+          <h2>Education</h2>
+          <EducationInput />
+        </div>
+        <div>
+          <h2>Work Experience</h2>
+          <WorkXpInput />
+        </div>
       </form>
     </>
   );
