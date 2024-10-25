4c5a80376b4eaabee6a86a952ba09f5c28fa043f
Alex <alexvnep@gmail.com>
2024-10-25 13:47:07 +0200

    feat: create label and inputs foe personal details

---
+++ src/components/PersonalDetailsInput.jsx
@@ -0,0 +1,16 @@
+function PersonalDetailsInput() {
+  return (
+    <>
+      <label htmlFor="name">Name: </label>
+      <input type="text" id="name" />
+      <label htmlFor="surname">Surname: </label>
+      <input type="text" id="surname" />
+      <label htmlFor="email">Email: </label>
+      <input type="email" name="email" id="email" />
+      <label htmlFor="tel">Phone: </label>
+      <input type="tel" name="tel" id="tel" />
+    </>
+  );
+}
+
+export default PersonalDetailsInput;
