43d28e8905642ae20a69d06ec92c21c8c6a9482c
Alex <alexvnep@gmail.com>
2024-10-28 09:43:01 +0200

    refactor: to use the new InputField comp

--- src/components/PersonalDetailsInput.jsx
+++ src/components/PersonalDetailsInput.jsx
@@ -1,17 +1,49 @@
-import Button from "./Button";
+import InputField from "./InputField";
+// import Button from "./Button";
 
-function PersonalDetailsInput() {
+function PersonalDetailsInput({ formData, onChange }) {
   return (
     <>
-      <label htmlFor="name">Name: </label>
-      <input type="text" id="name" />
-      <label htmlFor="surname">Surname: </label>
+      <InputField
+        type="text"
+        value={formData.fname}
+        placeholder="First Name"
+        label="First Name: "
+        name="fname"
+        onChange={onChange}
+      />
+      <InputField
+        type="text"
+        value={formData.lname}
+        placeholder="Last Name"
+        label="Surname: "
+        name="lname"
+        onChange={onChange}
+      />
+      <InputField
+        type="email"
+        value={formData.email}
+        placeholder="Email"
+        label="Email: "
+        name="email"
+        onChange={onChange}
+      />
+      <InputField
+        type="tel"
+        value={formData.tel}
+        placeholder="012 345 6789"
+        label="Phone: "
+        name="tel"
+        onChange={onChange}
+      />
+
+      {/* <label htmlFor="surname">Surname: </label>
       <input type="text" id="surname" />
       <label htmlFor="email">Email: </label>
       <input type="email" name="email" id="email" />
       <label htmlFor="tel">Phone: </label>
       <input type="tel" name="tel" id="tel" />
-      <Button text="Edit" />
+      <Button text="Edit" /> */}
     </>
   );
 }
