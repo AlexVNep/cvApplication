bd8cea5cd569eed5338a0e32fd1bc7a241c9b0c0
Alex <alexvnep@gmail.com>
2024-10-28 08:43:05 +0200

    chore: change tel to date type

--- src/components/EducationInput.jsx
+++ src/components/EducationInput.jsx
@@ -10,7 +10,7 @@ function EducationInput() {
       <label htmlFor="study-start">Year Started: </label>
       <input type="date" name="study-start" id="study-start" />
       <label htmlFor="study-end">Year Graduated: </label>
-      <input type="tel" name="study-end" id="study-end" />
+      <input type="date" name="study-end" id="study-end" />
       <Button text="Edit" />
     </>
   );
